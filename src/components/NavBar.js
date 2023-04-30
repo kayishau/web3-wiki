0c4a13f8b839b2250a6f1d4249d4fabc09f373d4
kayisha Ulysse <kayishaulysse@kayishas-MBP.fios-router.home>
2023-04-30 19:53:43 -0400

    routing working again

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -18,26 +18,25 @@ const NavBar = () => {
     // style={{ writingMode: 'vertical-rl' }} 
   return (
     <header className="w-1/6 min-h-screen px-22 bg-hotOrange ">
-        <nav>
-        {/* transform -rotate-90 [writing-mode:vertical-lr] ... */}
-            {/* <div className="border border-black"> */}
-            <div className="flex flex-col items-center gap-40 p-20 text-3xl font-bold  ">
+    <nav>
+    {/* transform -rotate-90 [writing-mode:vertical-lr] ... */}
+        {/* <div className="border border-black"> */}
+        <div className="flex flex-col items-center gap-40 p-20 text-3xl font-bold  ">
 
-            <div className="h-20 w-20 transform -rotate-90 hover:underline">
-                    <CustomLink href="/resources" title="Extra"/>
-                </div>
-                <div className="h-20 w-20 transform -rotate-90 hover:underline ">
-                    <CustomLink href="/topic" title="Topics"/>
-                </div>
-                <div className="h-20 w-20 transform -rotate-90 hover:underline">
-                     <CustomLink href="/overview" title="Overview"/>
-                </div>
-                <div className="h-20 w-20 transform -rotate-90 hover:underline">
-                    <CustomLink href="/" title="Home"/>
-                </div>
-
+        <div className="h-20 w-20 transform -rotate-90 hover:underline ">
+                <Link href="/resources">Extra</Link>
             </div>
-               
+            <div className="h-20 w-20 transform -rotate-90 hover:underline ">
+                <Link href="/topic">Topics</Link>
+            </div>
+            <div className="h-20 w-20 transform -rotate-90 hover:underline ">
+                 <Link href="/overview">Overview</Link>
+            </div>
+            <div className="h-20 w-20 transform -rotate-90 hover:underline ">
+                <Link href="/">Home</Link>
+            </div>
+
+        </div>
           
          
             {/* </div> */}
